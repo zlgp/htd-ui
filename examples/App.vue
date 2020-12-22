e70b41220dd32a1b051f0a0b5461d40db042797d
steve <steve@htdtek.com>
2020-12-22 16:26:11 +0800

    first commit

--- examples/App.vue
+++ examples/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <htd-button type="success">成功按钮</htd-button>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
---
+++ packages/index.js
@@ -0,0 +1,21 @@
+// 整个包的入口
+import Button from './button.vue'
+import './fonts/iconfont.css'
+
+const components = [
+    Button
+]
+const install = function (Vue) {
+    // 全局注册所有组件
+    // 注册所有的组件
+    components.forEach(item => {
+        Vue.component(item.name, item)
+    })
+    // 判断是否直接引入文件，如果是，就不用调用Vue.use()
+    if (typeof window !== 'undefined' && window.Vue) {
+        install(window.Vue)
+    }
+
+}
+// vue插件必须保留install方法
+export default { install }
